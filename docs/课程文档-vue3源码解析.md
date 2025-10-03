a6ab95043396cb553685cec06f850f1210f29953
tyler4400 <tyler4400@foxmail.com>
2025-10-03 22:47:14 +0800

    深度代理对象

--- docs/课程文档-vue3源码解析.md
+++ docs/课程文档-vue3源码解析.md
@@ -1,3 +1,5 @@
+学习来源1：
+来源于抖音课程
 源码地址：https://github.com/vue3-camp/vue3
 私我 github 用户名加入组织后，可查看源码
 
@@ -7,3 +9,14 @@
 文档md: https://github.com/vue3-camp/vue3/blob/docs/Vue3%20%E6%BA%90%E7%A0%81%E8%A7%A3%E6%9E%90.md
 
 有需要 obsidian 的同学在 源码仓库 docs 分支可以获取到全部的文档内容
+
+
+
+学习来源2：
+【2025最新版 Vue3.4 核心源码解析教程（全65集）】
+https://www.bilibili.com/video/BV1zBc2e4EaZ
+
+源码地址：
+https://github.com/ijntvwh/vue3-study.git
+
+
--- packages/reactivity3.4/src/baseHandler.ts
+++ packages/reactivity3.4/src/baseHandler.ts
@@ -1,4 +1,6 @@
 import { track, trigger } from './reactiveEffect'
+import { isObject } from '@vue/shared'
+import { reactive } from '@vue/reactivity3.4'
 
 export enum ReactiveFlags {
   IS_REACTIVE = '__v_isReactive', // 可以使用symbol
@@ -8,13 +10,18 @@ export const mutableHandlers: ProxyHandler<object> = {
   get(target, key, receiver) {
     if (key === ReactiveFlags.IS_REACTIVE) return true
 
-    // 依赖收集 todo 应该让响应式属性 和 effect 映射起来
+    // 依赖收集
     track(target, key)
 
-    return Reflect.get(target, key, receiver)
+    const res = Reflect.get(target, key, receiver)
+    if (isObject(res)) {
+      // 当取的值也是对象的时候,需要对这个对象进行代理,递归代理
+      return reactive(res)
+    }
+    return res
   },
   set(target, key, value, receiver) {
-    // 触发更新 todo 找到属性 让对应的effect重新执行
+    // 触发更新
 
     const oldVal = target[key]
 
